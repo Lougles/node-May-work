d16d3e989bcaacc63ffdd255d36104e49fff04c4
vovacelidze <louglescom@gmail.com>
2022-05-30 15:33:32 +0300

    method addUser is done

--- src/controllers/usersController.js
+++ src/controllers/usersController.js
@@ -1,134 +1,132 @@
-const {schema, joiName, joiEmail, joiPhone, joiFavorite} = require('../middlewares/validation');
-const { ObjectID } = require('bson');
-
+const {User} = require('../db/userModel')
 
 const getUsers = async (req, res, next) => {
-  const result = await req.db.Users.find({}).toArray();
+  const result = await User.find({});
     res.json({
     status: 'success',
     data: result,
   })
 }
 
-const getUserbyId = async (req, res, next) => {
-  const id = new ObjectID(req.params.contactId);
-  const result = await req.db.Users.findOne({_id: id}, {
-    name: 1,
-    email: 1,
-    phone: 1,
-  });
-  if(result){
-    res.json({
-      status: "success",
-      data: result
-    })
-  }else{
-    res.status(404).json({
-      status: "Not Found"
-    });
-  }
+const getUserbyId = async (req, res) => {
+  // const id = new ObjectID(req.params.contactId);
+  // const result = await req.db.Users.findOne({_id: id}, {
+  //   name: 1,
+  //   email: 1,
+  //   phone: 1,
+  // });
+  // if(result){
+  //   res.json({
+  //     status: "success",
+  //     data: result
+  //   })
+  // }else{
+  //   res.status(404).json({
+  //     status: "Not Found"
+  //   });
+  // }
 }
 
-const deleteUser = async (req, res, next) => {
-  const id = new ObjectID(req.params.contactId);
-  const result = await req.db.Users.remove({_id: id})
-  if(result){
-    res.json({
-      status: "success",
-      data: result,
-    })
-  }else {
-    res.status(404).json({
-      status: "Not Found"
-    });
-  }
+const deleteUser = async (req, res) => {
+  // const id = new ObjectID(req.params.contactId);
+  // const result = await req.db.Users.remove({_id: id})
+  // if(result){
+  //   res.json({
+  //     status: "success",
+  //     data: result,
+  //   })
+  // }else {
+  //   res.status(404).json({
+  //     status: "Not Found"
+  //   });
+  // }
 }
 
-const postUser = async (req, res, next) => {
-  const data = await schema.validateAsync(req.body);
-  const result = await req.db.Users.insertOne(data);
-  if(result){
-    res.json({
-      status: 'success',
-      data: result
-    })
-  }else {
-    res.json({
-      status: "Fail",
-    })
-  }
+const postUser = async (req, res) => {
+  // const {name, email, phone, favorite} = await schema.validateAsync(req.body);
+  // const result = await req.db.Users.insertOne(data);
+  // if(result){
+  //   res.json({
+  //     status: 'success',
+  //     data: result
+  //   })
+  // }else {
+  //   res.json({
+  //     status: "Fail",
+  //   })
+  // }
 }
 
-const updateAllfields = async (req, res, next) => {
-  const id = new ObjectID(req.params.contactId);
-  const {name, email, phone, favorite} = await schema.validateAsync(req.body);
-  const result = await req.db.Users
-  .findOneAndUpdate({_id: id}, { $set: {name, email, phone, favorite}});
-  if (result){
-    res.json({
-      status: "Success",
-      data: result,
-    })
-  }else {
-    res.sendStatus(304);
-  }
+const updateAllfields = async (req, res) => {
+  // const id = new ObjectID(req.params.contactId);
+  // const {name, email, phone, favorite} = await schema.validateAsync(req.body);
+  // const result = await req.db.Users
+  // .findOneAndUpdate({_id: id}, { $set: {name, email, phone, favorite}});
+  // if (result){
+  //   res.json({
+  //     status: "Success",
+  //     data: result,
+  //   })
+  // }else {
+  //   res.sendStatus(304);
+  // }
 }
 
-const updateFavorite = async (req, res, next) => {
-  const {favorite} = await joiFavorite.validateAsync(req.body);
-  const id = new ObjectID(req.params.contactId);
-  const result = await req.db.Users.
-  findOneAndUpdate({_id: id},{ $set: {favorite}});
-  if (result) {
-    res.json({
-      status: "Success",
-      data: result,
-    })
-  }else {
-    res.sendStatus(304);
-  }
+const updateFavorite = async (req, res) => {
+  // const {favorite} = await joiFavorite.validateAsync(req.body);
+  // const id = new ObjectID(req.params.contactId);
+  // const result = await req.db.Users.
+  // findOneAndUpdate({_id: id},{ $set: {favorite}});
+  // if (result) {
+  //   res.json({
+  //     status: "Success",
+  //     data: result,
+  //   })
+  // }else {
+  //   res.sendStatus(304);
+  // }
 }
 
-const updatePhone = async (req, res, next) => {
-  const {phone} = await joiPhone.validateAsync(req.body);
-  const id = new ObjectID(req.params.contactId);
-  const result = await req.db.Users.findOneAndUpdate({_id: id},{ $set: {phone}});
-  if (result) {
-    res.json({
-      status: "Success",
-      data: result,
-    })
-  }else {
-    res.sendStatus(304);
-  }
+const updatePhone = async (req, res) => {
+  // const {phone} = await joiPhone.validateAsync(req.body);
+  // const id = new ObjectID(req.params.contactId);
+  // const result = await req.db.Users.findOneAndUpdate({_id: id},{ $set: {phone}});
+  // if (result) {
+  //   res.json({
+  //     status: "Success",
+  //     data: result,
+  //   })
+  // }else {
+  //   res.sendStatus(304);
+  // }
 }
 
-const updateEmail = async (req, res, next) => {
-  const {email} = await joiEmail.validateAsync(req.body);
-  const id = new ObjectID(req.params.contactId);
-  const result = await req.db.Users.findOneAndUpdate({_id: id},{ $set: {email}});
-  if (result) {
-    res.json({
-      status: "Success",
-      data: result,
-    })
-  }else {
-    res.sendStatus(304);
-  }
+const updateEmail = async (req, res) => {
+  // const {email} = await joiEmail.validateAsync(req.body);
+  // const id = new ObjectID(req.params.contactId);
+  // const result = await req.db.Users.findOneAndUpdate({_id: id},{ $set: {email}});
+  // if (result) {
+  //   res.json({
+  //     status: "Success",
+  //     data: result,
+  //   })
+  // }else {
+  //   res.sendStatus(304);
+  // }
 }
 
-const updateName = async (req, res, next) => {
-  const {name} = await joiName.validateAsync(req.body);
-  const id = new ObjectID(req.params.contactId);
-  const result = await req.db.Users.findOneAndUpdate({_id: id},{ $set: {name}});
-  if (result) {
-    res.json({
-      status: "Success",
-      data: result,
-    })
-  }else {
-    res.sendStatus(304);
-  }
+const updateName = async (req, res) => {
+  // const {name} = await joiName.validateAsync(req.body);
+  // const id = new ObjectID(req.params.contactId);
+  // const result = await req.db.Users.findOneAndUpdate({_id: id},{ $set: {name}});
+  // if (result) {
+  //   res.json({
+  //     status: "Success",
+  //     data: result,
+  //   })
+  // }else {
+  //   res.sendStatus(304);
+  // }
 }
 
 
@@ -144,5 +142,4 @@ module.exports = {
   updatePhone,
   updateEmail,
   updateName,
-
 }
