d0d85935afef7acd8889be90a325c76944aceebc
vovacelidze <louglescom@gmail.com>
2022-05-30 13:31:53 +0300

    minor edits

--- server.js
+++ server.js
@@ -5,11 +5,15 @@ require('dotenv').config()
 
 const PORT = process.env.PORT || 4040
 const start = async () =>  {
-  await connectMongo();
-    app.listen(PORT, (err) => {
-    if (err) console.log('error', err);
-    console.log(`Server running. Use our API on port: ${PORT}`)
-  })
+  try{
+    await connectMongo();
+      app.listen(PORT, (err) => {
+      if (err) console.log('error', err);
+      console.log(`Server running. Use our API on port: ${PORT}`)
+    })
+  }catch(err) {
+console.error(`Failed to start app with error:  ${err.message}`)
+  }
 }
 
 start()
