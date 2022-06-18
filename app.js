743c85faab7b4266b16befd8ffdf07de167b0efe
vovacelidze <louglescom@gmail.com>
2022-06-18 15:20:12 +0300

    last fix

--- app.js
+++ app.js
@@ -1,7 +1,6 @@
 const express = require('express')
 const logger = require('morgan')
 const cors = require('cors')
-
 const contactsRouter = require('./src/routes/api/contacts');
 const authRouter = require('./src/routes/api/auth');
 const {errorHandler} = require('./src/helpers/trycatchHelper');
