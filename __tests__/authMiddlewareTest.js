b53d4e229ab7de8ef74a67dee96a773a8ac4e35c
vovacelidze <louglescom@gmail.com>
2022-08-13 11:29:09 +0300

    added auth test for next mistake

--- __tests__/authMiddlewareTest.js
+++ __tests__/authMiddlewareTest.js
@@ -1,9 +1,10 @@
 // const { describe } = require("yargs");
 const {authMiddleware} = require('../src/middlewares/authMiddleware');
+const {NotAuthorizedError} = require('../src/helpers/errors')
 const jwt = require('jsonwebtoken');
 require('dotenv').config();
 
-describe('Auth middleware test', ()=> {
+describe('Auth middleware test', () => {
   it('should call next() and add user and token properties to req object', () => {
     const user = {
       _id: '1',
@@ -13,21 +14,30 @@ describe('Auth middleware test', ()=> {
       _id: user._id,
       createdAt: user.createdAt,
     }, process.env.JWT_SECRET);
-
     const mReq = {
       headers: {
         'authorization': `Bearer ${token}`,
       }
     };
+
     const mRes = {};
     const mockNext = jest.fn();
 
     authMiddleware(mReq, mRes, mockNext);
-
     expect(mReq.token).toEqual(token);
     expect(mReq.user._id).toEqual(user._id);
     expect(mReq.user.createdAt).toEqual(user.createdAt);
     expect(mockNext).toHaveBeenCalled();
+  })
+  it('should call next() with error in authorization header is absent ', () => {
+    const mReq = {
+      headers: {}
+    };
 
+    const mRes = {};
+    const mockNext = jest.fn();
+    authMiddleware(mReq, mRes, mockNext);
+    expect(mockNext).toHaveBeenCalledWith(new NotAuthorizedError('Please provide a token!'));
   })
 })
+
